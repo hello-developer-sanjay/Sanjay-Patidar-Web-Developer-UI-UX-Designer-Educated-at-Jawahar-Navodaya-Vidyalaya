992b8efffb17cba99f79f8240c28be8e7d7ea2aa
hello-developer-sanjay <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-17 22:29:59 +0530

    Update ProjectDetails.jsx

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -181,7 +181,7 @@ const ProjectDetails = () => {
   useEffect(() => {
     async function fetchProjectDetails() {
       try {
-        const response = await axios.get(`http://localhost:5000/api/projects/details/${id}`);
+        const response = await axios.get(`https://portfolio-back-dujw.onrender.com/api/projects/details/${id}`);
         setProject(response.data);
       } catch (error) {
         console.error('Error fetching project details:', error);
