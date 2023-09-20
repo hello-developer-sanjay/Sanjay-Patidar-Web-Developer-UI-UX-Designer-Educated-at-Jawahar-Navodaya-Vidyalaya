83da75bf4f5c9f8ed0446e0a2189c056efa47acc
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-21 01:55:17 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,42 +4,47 @@ import { FaHome, FaFolder } from 'react-icons/fa';
 import styled from 'styled-components';
 
 const Nav = styled.nav`
-  background-color: #1e3a5f; /* Updated background color */
+  background-color: #333;
   color: #fff;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 1.5rem;
+  padding: 1rem;
   position: sticky;
   top: 0;
   z-index: 1000;
   box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
-  backdrop-filter: blur(10px); /* Adds a frosted glass effect to the background */
 `;
 
 const Logo = styled.h1`
-  font-size: 1rem; /* Increased font size */
+  font-size: 2rem;
   font-weight: bold;
   margin: 0;
   text-transform: uppercase;
-  letter-spacing: 1px; /* Increased letter spacing */
+  letter-spacing: 2px;
 `;
 
 const NavList = styled.ul`
   list-style: none;
   display: flex;
-  gap: 1rem; /* Increased gap between items */
+  gap: 1rem;
 `;
 
 const NavItem = styled.li`
   position: relative;
+
+  &:hover {
+    .SubNavList {
+      opacity: 1;
+      visibility: visible;
+    }
+  }
 `;
 
 const NavLinkStyled = styled(NavLink)`
   text-decoration: none;
   color: #fff;
   font-weight: bold;
-  font-size: 1.2rem; /* Increased font size */
 
   &:hover {
     text-decoration: underline;
@@ -67,6 +72,7 @@ const SubNavList = styled.ul`
   padding: 0.5rem 0;
   gap: 0.5rem;
   border-radius: 8px;
+  /* Adjust the position to cover both Home and Projects links */
   top: calc(100% + 8px);
   left: 0;
   transform: translateX(0%);
@@ -76,27 +82,22 @@ const SubNavItem = styled.li``;
 
 const SubNavLinkStyled = styled(NavLink)`
   text-decoration: none;
+  color: #fff;
   padding: 0.5rem 1rem;
   transition: background-color 0.3s;
-  display: inline-block;
-  background: linear-gradient(to bottom, transparent 0%, transparent 50%, #fff 50%, #fff 100%);
-  background-size: 100% 200%;
-  background-position: 0 100%;
-  color: #333;
+  border: 2px solid #fff;
+  border-radius: 4px;
+  font-weight: bold;
 
-  &:hover {
-    background-position: 0 0;
-    color: #fff;
+  &:not(:last-child) {
+    margin-bottom: 0.5rem;
   }
 
-  &.web {
-    background-image: linear-gradient(to bottom, transparent 0%, transparent 50%, #ff5722 50%, #ff5722 100%);
-  }
-
-  &.projects {
-    background-image: linear-gradient(to bottom, transparent 0%, transparent 50%, #2196F3 50%, #2196F3 100%);
+  &:focus {
+    outline: none;
   }
 `;
+
 
 const Navbar = () => {
   return (
@@ -114,7 +115,7 @@ const Navbar = () => {
           </NavLinkStyled>
           <SubNavList className="SubNavList">
             <SubNavItem>
-              <SubNavLinkStyled to="/projects/web" className="web">Web Projects</SubNavLinkStyled>
+              <SubNavLinkStyled to="/projects/web">Web Projects</SubNavLinkStyled>
             </SubNavItem>
             <SubNavItem>
               <SubNavLinkStyled to="/projects/mobile">Mobile Projects</SubNavLinkStyled>
