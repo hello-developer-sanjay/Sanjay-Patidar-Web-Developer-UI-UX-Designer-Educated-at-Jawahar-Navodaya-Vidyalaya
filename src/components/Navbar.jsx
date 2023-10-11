27edbf6cc23390aa73595e1f839802e9e88b5223
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-10-11 23:43:53 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { FaHome, FaFolder } from 'react-icons/fa';
 import styled from 'styled-components';
 
