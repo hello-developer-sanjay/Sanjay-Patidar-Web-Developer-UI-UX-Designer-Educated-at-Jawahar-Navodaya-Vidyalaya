5eca28fb5fd92be83cf6b382f9baaeda91700484
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-10-25 21:09:40 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -142,7 +142,7 @@ const Sidebar = () => {
     async function fetchCertifications() {
       try {
         const response = await axios.get(
-          "https://port-back-gbq9.onrender.com/api/certifications"
+          "https://portfolio-backend-lp9q.onrender.com/api/certifications"
         );
         setCertifications(response.data);
       } catch (error) {
