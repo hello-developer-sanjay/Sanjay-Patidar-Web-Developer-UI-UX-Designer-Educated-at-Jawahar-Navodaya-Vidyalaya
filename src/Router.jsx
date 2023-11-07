3177bf335ad2244332f99b9ab81db31e7d6da0a8
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-11-07 12:57:40 +0530

    Update Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import  { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route , useLocation } from "react-router-dom";
+import PageTransition from "./components/PageTransition"; // Import your PageTransition component
 import Home from "./pages/Home";
 import Projects from "./components/Projects";
 import Skills from "./pages/Skills";
@@ -13,30 +14,36 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import ProjectDetails from './components/ProjectDetails';
+import Education from "./components/Education";
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to the top of the page when the route changes.
+  }, [pathname]);
+
+  return null; // This component doesn't render anything.
+};
 const AppRouter = () => {
+ 
   return (
     <Router>
       <Navbar />
       <Sidebar />
+      <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/projects/:category" element={<Projects />} />
-        <Route path="/api/projects/details/:id" element={<ProjectDetails />} />
- {/* New route for ProjectDetails */}
-
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/experiences" element={<Experiences />} />
-        <Route path="/certifications" element={<Certifications />} />
-        <Route
-          path="/certifications/:title"
-          element={<CertificationDetails />}
-        />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/blogs" element={<Blogs />} />
-
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<PageTransition><Home /></PageTransition>} />
+        <Route path="/projects" element={<PageTransition><Projects /></PageTransition>} />
+        <Route path="/projects/:category" element={<PageTransition><Projects /></PageTransition>} />
+        <Route path="/api/projects/details/:id" element={<PageTransition><ProjectDetails /></PageTransition>} />
+        <Route path="/skills" element={<PageTransition><Skills /></PageTransition>} />
+        <Route path="/experiences" element={<PageTransition><Experiences /></PageTransition>} />
+        <Route path="/certifications" element={<PageTransition><Certifications /></PageTransition>} />
+        <Route path="/certifications/:title" element={<PageTransition><CertificationDetails /></PageTransition>} />
+        <Route path="/resume" element={<PageTransition><Resume /></PageTransition>} />
+        <Route path="/blogs" element={<PageTransition><Blogs /></PageTransition>} />
+        <Route path="/education" element={<PageTransition><Education /></PageTransition>} />
+        <Route path="*" element={<PageTransition><NotFound /></PageTransition>} />
       </Routes>
       <Footer />
     </Router>
