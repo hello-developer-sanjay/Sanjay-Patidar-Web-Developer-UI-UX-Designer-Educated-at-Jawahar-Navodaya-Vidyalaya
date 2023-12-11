675fa0368036923cbda8d790acda8e57ae22cc13
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-12-11 23:26:56 +0530

    Update ProtectedPage.jsx

--- src/components/ProtectedPage.jsx
+++ src/components/ProtectedPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import  { keyframes } from 'styled-components';
@@ -80,6 +80,15 @@ const StyledWrapper = styled.div`
     padding: 0;
     margin-top: 20px;
   }
+  li.resolved {
+    background-color: #d3ffd3; /* You can customize the background color for resolved queries */
+  }
+
+  span.resolve-button {
+    cursor: pointer;
+    color: #3498db;
+    margin-left: 10px;
+  }
 
   li {
     border: 1px solid #ddd;
@@ -90,6 +99,7 @@ const StyledWrapper = styled.div`
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   }
 `;
+
 const gradientAnimation = keyframes`
   0% {
     background-position: 0% 50%;
@@ -150,7 +160,15 @@ const UnlockButton = styled.button`
     color: #ff7e5f;
   }
 `;
+const ResolvedSpan = styled.span`
+  cursor: pointer;
+  color: #3498db;
+  margin-left: 10px;
 
+  &:hover {
+    color: #1abc9c;
+  }
+`;
 const ProtectedPage = () => {
     const [password, setPassword] = useState('');
     const [passwordVisible, setPasswordVisible] = useState(false);
@@ -158,94 +176,120 @@ const ProtectedPage = () => {
     const [userProfiles, setUserProfiles] = useState([]);
     const [feedbacks, setFeedbacks] = useState([]);
     const [queries, setQueries] = useState([]);
-  const handlePasswordSubmit = async () => {
-    try {
-      const response = await axios.post('https://portfolio-back-aruc.onrender.com/api/authenticate', { password });
-      if (response.data.authenticated) {
-        setAuthenticated(true);
-
-        // Fetch feedbacks after successful authentication
-        const feedbacksResponse = await axios.get('https://portfolio-back-aruc.onrender.com/api/feedbacks');
-        setFeedbacks(feedbacksResponse.data);
-
-        // Fetch queries after successful authentication
-        const queriesResponse = await axios.get('https://portfolio-back-aruc.onrender.com/api/queries');
-        setQueries(queriesResponse.data);
-// Fetch user profiles after successful authentication
-const userProfilesResponse = await axios.get('https://portfolio-back-aruc.onrender.com/api/userprofiles');
-          
-// Ensure that the data received is an array before setting the state
-if (Array.isArray(userProfilesResponse.data)) {
-  setUserProfiles(userProfilesResponse.data);
-} else {
-  console.error('Invalid data structure received for user profiles');
-}      } else {
-        console.log('Authentication failed');
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          // Fetch queries after successful authentication
+          const queriesResponse = await axios.get('https://portfolio-back-aruc.onrender.com/api/queries');
+          setQueries(queriesResponse.data.map(query => ({ ...query, resolved: false })));
+  
+          // Fetch user profiles after successful authentication
+          const userProfilesResponse = await axios.get('https://portfolio-back-aruc.onrender.com/api/userprofiles');
+          if (Array.isArray(userProfilesResponse.data)) {
+            setUserProfiles(userProfilesResponse.data);
+          } else {
+            console.error('Invalid data structure received for user profiles');
+          }
+        } catch (error) {
+          console.error('Error during data fetching:', error);
+        }
+      };
+  
+      if (authenticated) {
+        fetchData();
       }
-    } catch (error) {
-      console.error('Error during authentication:', error);
-    }
+    }, [authenticated]);
+  
+    const handlePasswordSubmit = async () => {
+      try {
+        const response = await axios.post('https://portfolio-back-aruc.onrender.com/api/authenticate', { password });
+        if (response.data.authenticated) {
+          setAuthenticated(true);
+  
+          // Fetch feedbacks after successful authentication
+          const feedbacksResponse = await axios.get('https://portfolio-back-aruc.onrender.com/api/feedbacks');
+          setFeedbacks(feedbacksResponse.data);
+        } else {
+          console.log('Authentication failed');
+        }
+      } catch (error) {
+        console.error('Error during authentication:', error);
+      }
+    };
+  
+    const togglePasswordVisibility = () => {
+      setPasswordVisible(!passwordVisible);
+    };
+  
+    const markAsResolved = (queryId) => {
+      setQueries((prevQueries) =>
+        prevQueries.map((query) =>
+          query._id === queryId ? { ...query, resolved: true } : query
+        )
+      );
+    };
+  
+    return (
+      <StyledWrapper>
+        {!authenticated ? (
+          <>
+            <h1>Unlock the Secrets!</h1>
+            <div className="password-container">
+              <input
+                type={passwordVisible ? 'text' : 'password'}
+                placeholder="Enter the secret password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <UnlockButton className="toggle-button" onClick={togglePasswordVisibility}>
+                {passwordVisible ? '🙈' : '👁️'}
+              </UnlockButton>
+            </div>
+            <StyledButton onClick={handlePasswordSubmit}>Open Sesame </StyledButton>
+          </>
+        ) : (
+          <>
+            {/* Display user details along with feedbacks and queries */}
+            <h1>Dashboard</h1>
+            <p>User Details :</p>
+            <ul>
+              {userProfiles.map((profile) => (
+                <li key={profile._id.$oid}>
+                  <strong>Email: {profile.email}</strong>
+                  <p>Username: {profile.username}</p>
+                  <p>Last Sign In: {profile.lastSignInAt}</p>
+                  {/* Add more profile details as needed */}
+                </li>
+              ))}
+            </ul>
+  
+            <h1>Feedbacks</h1>
+            <ul>
+              {feedbacks.map((feedback) => (
+                <li key={feedback._id}>
+                  <strong>UserName: {feedback.name} || Email id : {feedback.email} :</strong> {feedback.feedback}
+                </li>
+              ))}
+            </ul>
+  
+            <h1>Queries</h1>
+            <ul>
+              {queries.map((query) => (
+                <li key={query._id} className={query.resolved ? 'resolved' : ''}>
+                  <strong>UserName: {query.name} || Email id : {query.email} :</strong> {query.query}
+                  {!query.resolved && (
+                    <ResolvedSpan onClick={() => markAsResolved(query._id)}>
+                      &#10003; Resolve
+                    </ResolvedSpan>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </StyledWrapper>
+    );
   };
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible);
-  };
-
-  return (
-    <StyledWrapper>
-    {!authenticated ? (
-      <>
-        <h1>Unlock the Secrets!</h1>
-        <div className="password-container">
-          <input
-            type={passwordVisible ? 'text' : 'password'}
-            placeholder="Enter the secret password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <UnlockButton className="toggle-button" onClick={togglePasswordVisibility}>
-            {passwordVisible ? '🙈' : '👁️'}
-          </UnlockButton>
-        </div>
-        <StyledButton onClick={handlePasswordSubmit}>Open Sesame </StyledButton>
-      </>
-    ) : (
-
-        <>{/* Display user details along with feedbacks and queries */}
-        
-        
-        <h1>Dashboard</h1>
-        <p >User Details : </p>
-        <ul>
-            {userProfiles.map((profile) => (
-              <li key={profile._id.$oid}>
-                <strong>Email: {profile.email}</strong>
-                <p>Username: {profile.username}</p>
-                <p>Last Sign In: {profile.lastSignInAt}</p>
-                {/* Add more profile details as needed */}
-              </li>
-            ))}
-          </ul>
-
-  <h1>Feedbacks</h1>
-          <ul>
-            {feedbacks.map((feedback) => (
-              <li key={feedback._id}>
-                <strong>UserName: {feedback.name} || Email id : {feedback.email} :</strong> {feedback.feedback}
-              </li>
-            ))}
-          </ul>
-          <h1>Queries</h1>
-          <ul>
-            {queries.map((query) => (
-              <li key={query._id}>
-                <strong>UserName: {query.name} || Email id : {query.email} :</strong> {query.query}
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
-    </StyledWrapper>
-  );
-};
-
-export default ProtectedPage;
+  
+  export default ProtectedPage;
