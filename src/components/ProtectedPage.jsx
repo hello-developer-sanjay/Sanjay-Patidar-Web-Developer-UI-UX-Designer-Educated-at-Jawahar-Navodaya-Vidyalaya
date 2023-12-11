11262a58c3086900445d53fbc5e46a77ea4909bc
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-12-11 21:22:42 +0530

    Update ProtectedPage.jsx

--- src/components/ProtectedPage.jsx
+++ src/components/ProtectedPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+import  { keyframes } from 'styled-components';
 
 const StyledWrapper = styled.div`
   max-width: 600px;
@@ -20,7 +21,7 @@ const StyledWrapper = styled.div`
   input {
     padding: 12px;
     font-size: 16px;
-    margin-right: 10px;
+    margin-right: 5px;
     width: 60%;
     max-width: 300px;
     border: 1px solid #ddd;
@@ -60,6 +61,66 @@ const StyledWrapper = styled.div`
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   }
 `;
+const gradientAnimation = keyframes`
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+`;
+
+const StyledButton = styled.button`
+  position: relative;
+  overflow: hidden;
+  padding: 15px 30px;
+  font-size: 16px;
+  font-weight: bold;
+  color: #fff;
+  background: linear-gradient(45deg, #ff7e5f, #feb47b);
+  background-size: 200% 200%;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  transition: transform 0.3s ease;
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(45deg, #ff7e5f, #feb47b);
+    background-size: 200% 200%;
+    opacity: 0.5;
+    z-index: -1;
+    animation: ${gradientAnimation} 4s ease infinite;
+  }
+
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 10px 20px rgba(255, 126, 95, 0.3);
+  }
+`;
+
+const UnlockButton = styled.button`
+  margin-left: 10px;
+  padding: 12px;
+  cursor: pointer;
+  border: none;
+  background: none;
+  font-size: 20px;
+  color: #555;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: #ff7e5f;
+  }
+`;
 
 const ProtectedPage = () => {
     const [password, setPassword] = useState('');
@@ -95,23 +156,24 @@ const ProtectedPage = () => {
 
   return (
     <StyledWrapper>
-      {!authenticated ? (
-        <>
-          <h1>Unlock the Secrets!</h1>
-          <div className="password-container">
-            <input
-              type={passwordVisible ? 'text' : 'password'}
-              placeholder="Enter the secret password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button className="toggle-button" onClick={togglePasswordVisibility}>
-              {passwordVisible ? '🙈' : '👁️'}
-            </button>
-          </div>
-          <button onClick={handlePasswordSubmit}>Open Sesame</button>
-        </>
-      ) : (
+    {!authenticated ? (
+      <>
+        <h1>Unlock the Secrets!</h1>
+        <div className="password-container">
+          <input
+            type={passwordVisible ? 'text' : 'password'}
+            placeholder="Enter the secret password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <UnlockButton className="toggle-button" onClick={togglePasswordVisibility}>
+            {passwordVisible ? '🙈' : '👁️'}
+          </UnlockButton>
+        </div>
+        <StyledButton onClick={handlePasswordSubmit}>Open Sesame </StyledButton>
+      </>
+    ) : (
+
         <>
           <h1>Feedbacks</h1>
           <ul>
