39150276d953f723f36b9bb11e5684a78037b75d
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-12-13 21:56:01 +0530

    Update Experiences.jsx

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -1,61 +1,129 @@
-// src/pages/Experiences.jsx
-import React from 'react';
-import styled from 'styled-components';
 
-const ExperiencesContainer = styled.div`
-  padding: 2rem;
-  min-height: 100vh;
-  background-color: #f0f3f4; /* Soft blue background */
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { FaReact } from "react-icons/fa";
+import { motion } from "framer-motion";
 
-const ExperienceCard = styled.div`
-  background-color: #ffffff; /* White card background */
-  border-radius: 10px;
-  padding: 1.5rem;
-  margin-bottom: 1.5rem; /* Increased spacing between cards */
-  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1); /* Slightly enhanced shadow */
-  transition: transform 0.2s, box-shadow 0.2s;
+const experienceData = [
+  {
+    title: "React.js Developer",
+    company_name: "Starbucks",
+    iconBg: "linear-gradient(135deg, #4E50A0, #2B3252)",
+    date: "March 2020 - April 2021",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "Tech Innovations",
+    iconBg: "linear-gradient(135deg, #4E50A0, #2B3252)",
+    date: "May 2021 - Present",
+    points: [
+      "Building end-to-end web solutions with a focus on both front-end and back-end development.",
+      "Leading the integration of new technologies and frameworks to enhance overall project efficiency.",
+      "Collaborating with clients to understand project requirements and deliver tailored solutions.",
+      "Conducting training sessions for junior developers on best practices and new technologies.",
+    ],
+  },
+  {
+    title: "UI/UX Designer",
+    company_name: "Creative Designs Agency",
+    iconBg: "linear-gradient(135deg, #4E50A0, #2B3252)",
+    date: "January 2019 - February 2020",
+    points: [
+      "Designing user interfaces that prioritize usability, accessibility, and a seamless user experience.",
+      "Collaborating with development teams to implement responsive and visually appealing designs.",
+      "Conducting user research and usability testing to gather valuable feedback for design iterations.",
+      "Creating design prototypes and mockups for client presentations and project planning.",
+    ],
+  },
+  {
+    title: "Mobile App Developer",
+    company_name: "Innovate Mobile Solutions",
+    iconBg: "linear-gradient(135deg, #4E50A0, #2B3252)",
+    date: "June 2018 - December 2018",
+    points: [
+      "Developing native mobile applications for iOS and Android platforms using React Native.",
+      "Optimizing app performance and ensuring seamless integration with various mobile devices.",
+      "Collaborating with product managers to define app features and functionalities.",
+      "Conducting debugging and troubleshooting to ensure the stability of mobile applications.",
+    ],
+  },
+  // Add more experience entries as needed
+];
 
-  &:hover {
-    transform: translateY(-3px); /* Lift the card on hover */
-    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2); /* Enhanced shadow on hover */
-  }
-`;
+const pageVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 0.8, ease: "easeInOut" } },
+};
 
-const ExperienceTitle = styled.h3`
-  font-size: 1.8rem; /* Larger title font size */
-  margin-bottom: 0.5rem;
-  color: #333; /* Darker title color */
-`;
 
-const ExperienceDate = styled.p`
-  color: #777;
-  font-size: 1.2rem; /* Adjusted date font size */
-  margin-bottom: 1rem; /* Increased spacing below date */
-`;
+const entryVariants = {
+  hidden: { opacity: 0, x: -50 },
+  visible: { opacity: 1, x: 0, transition: { duration: 0.5, ease: "easeOut" } },
+};
 
-const ExperienceDescription = styled.p`
-  line-height: 1.6;
-  font-size: 1.4rem; /* Adjusted description font size */
-  color: #555; /* Slightly darker description text color */
-`;
-
-const Experiences = () => {
+const Experience = () => {
   return (
-    <ExperiencesContainer>
-      <ExperienceCard>
-        <ExperienceTitle>Software Developer at XYZ Company</ExperienceTitle>
-        <ExperienceDate>Jan 2020 - Present</ExperienceDate>
-        <ExperienceDescription>
-          Responsible for developing and maintaining web applications using React and Node.js.
-        </ExperienceDescription>
-      </ExperienceCard>
-      {/* Add more experience cards */}
-    </ExperiencesContainer>
+    <motion.div
+      className="experience"
+      initial="hidden"
+      animate="visible"
+      variants={pageVariants}
+      style={{ background: "#f5f5f5", padding: "50px 0" }} // Add background color and padding
+    >
+      <div className="central-ring"></div>
+      <VerticalTimeline>
+        {experienceData.map((experience, index) => (
+          <VerticalTimelineElement
+            key={index}
+            className={`vertical-timeline-element ${index % 2 === 0 ? 'right' : 'left'}`}
+            contentStyle={{
+              background: experience.iconBg,
+              color: "#fff",
+              boxShadow: "0 8px 16px rgba(0, 0, 0, 0.2)",
+              borderRadius: "20px",
+              padding: "20px",
+              border: "2px solid #fff",
+              backgroundClip: "padding-box", // To prevent the gradient from overlapping the border
+            }}
+            contentArrowStyle={{ borderRight: `10px solid ${experience.iconBg}`, borderLeft: `10px solid ${experience.iconBg}` }}
+            date={experience.date}
+            iconStyle={{ background: "#fff", color: experience.iconBg, boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)" }}
+            icon={<FaReact />}
+          >
+            <motion.h3
+              className="vertical-timeline-element-title"
+              variants={entryVariants}
+            >
+              {experience.title}
+            </motion.h3>
+            <motion.h4
+              className="vertical-timeline-element-subtitle"
+              variants={entryVariants}
+            >
+              {experience.company_name}
+            </motion.h4>
+            <ul>
+              {experience.points.map((point, pointIndex) => (
+                <motion.li
+                  key={pointIndex}
+                  variants={entryVariants}
+                  style={{ listStyleType: "none" }}
+                >
+                  {point}
+                </motion.li>
+              ))}
+            </ul>
+          </VerticalTimelineElement>
+        ))}
+      </VerticalTimeline>
+    </motion.div>
   );
 };
 
-export default Experiences;
+export default Experience;
