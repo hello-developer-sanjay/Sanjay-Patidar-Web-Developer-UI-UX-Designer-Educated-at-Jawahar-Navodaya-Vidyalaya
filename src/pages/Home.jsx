690913cf52d60d67c3f3db6fa45736a26546efb5
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-12-25 20:32:45 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -74,39 +74,52 @@ const BackgroundOverlay = styled.div`
   z-index: -1;
 `;
 
+
 const ProfileImage = styled(motion.img)`
   width: 180px;
   height: 180px;
   border-radius: 50%;
-  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);
-  animation: flip 5s infinite, glow 2s alternate infinite;
+  box-shadow: 0 0 10px rgba(255, 165, 0, 0.8), 0 0 20px rgba(255, 165, 0, 0.6);
+  transform-origin: center;
+  animation: heartbeat 1.5s infinite, rotateAndGlow 8s infinite, bounce 2s alternate infinite;
 
-  @keyframes flip {
+  @keyframes heartbeat {
     0%, 100% {
-      transform: rotateY(0deg);
-    }
-    25% {
-      transform: rotateY(90deg);
+      transform: scale(1);
     }
     50% {
-      transform: rotateY(180deg);
-    }
-    75% {
-      transform: rotateY(270deg);
+      transform: scale(1.1);
     }
   }
 
-  @keyframes glow {
-    0% {
-      box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);
+  @keyframes rotateAndGlow {
+    0%, 100% {
+      transform: rotate(0deg) scale(1);
+      box-shadow: 0 0 10px rgba(255, 165, 0, 0.8), 0 0 20px rgba(255, 165, 0, 0.6);
     }
-    100% {
-      box-shadow: 0px 0px 40px rgba(255, 165, 0, 0.8), 0px 0px 60px rgba(255, 165, 0, 0.6);
+    25% {
+      transform: rotate(90deg) scale(1.2);
+      box-shadow: 0 0 15px rgba(255, 165, 0, 0.9), 0 0 30px rgba(255, 165, 0, 0.7);
+    }
+    50% {
+      transform: rotate(180deg) scale(1);
+      box-shadow: 0 0 10px rgba(255, 165, 0, 0.8), 0 0 20px rgba(255, 165, 0, 0.6);
+    }
+    75% {
+      transform: rotate(270deg) scale(1.2);
+      box-shadow: 0 0 15px rgba(255, 165, 0, 0.9), 0 0 30px rgba(255, 165, 0, 0.7);
+    }
+  }
+
+  @keyframes bounce {
+    0%, 100% {
+      transform: translateY(0);
+    }
+    50% {
+      transform: translateY(-15px);
     }
   }
 `;
-
-
 const images = [profileImage1, profileImage2];
 let currentImageIndex = 0;
 
