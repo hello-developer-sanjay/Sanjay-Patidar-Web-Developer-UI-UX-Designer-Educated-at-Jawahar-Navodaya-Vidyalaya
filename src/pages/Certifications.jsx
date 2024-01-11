a5e4d46248cfc3159c984ce8a17e5e718c64a5e8
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-01-11 23:38:06 +0530

    Update Certifications.jsx

--- src/pages/Certifications.jsx
+++ src/pages/Certifications.jsx
@@ -1,10 +1,13 @@
 import  { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { css } from "@emotion/react";
+import { RingLoader } from "react-spinners";
 import "../styles/Certifications.css";
 
 const Certifications = () => {
   const [certifications, setCertifications] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchCertifications() {
@@ -13,38 +16,49 @@ const Certifications = () => {
           "https://portfolio-back-aruc.onrender.com/api/certifications"
         );
         setCertifications(response.data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching certifications:", error);
+        setLoading(false);
       }
     }
 
     fetchCertifications();
   }, []);
+  const override = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
 
   return (
     <div className="certifications-container">
-      {certifications.map((certification) => (
-        <div key={certification.id} className="certification-card">
-          <h3 className="certification-title">{certification.title}</h3>
-          <div className="certification-images">
-            {certification.imageUrl.map((url, index) => (
-              <img
-                key={index}
-                className="certification-image"
-                src={url}
-                alt={`Certification ${index + 1}`}
-              />
-            ))}
+      {loading ? (
+        <RingLoader color="#4b0082" loading={loading} css={override} size={150} />
+      ) : (
+        certifications.map((certification) => (
+          <div key={certification.id} className="certification-card">
+            <h3 className="certification-title">{certification.title}</h3>
+            <div className="certification-images">
+              {certification.imageUrl.map((url, index) => (
+                <img
+                  key={index}
+                  className="certification-image"
+                  src={url}
+                  alt={`Certification ${index + 1}`}
+                />
+              ))}
+            </div>
+            <Link
+              to={`/certifications/${encodeURIComponent(certification.title)}`}
+              className="certification-link"
+            >
+              Explore More
+            </Link>
           </div>
-          <Link
-            to={`/certifications/${encodeURIComponent(certification.title)}`}
-            className="certification-link"
-          >
-            Explore More
-          </Link>
-         
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
