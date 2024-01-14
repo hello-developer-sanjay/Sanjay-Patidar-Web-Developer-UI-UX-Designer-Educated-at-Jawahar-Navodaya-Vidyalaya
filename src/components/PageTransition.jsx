b422f925ad191598c2da8edf324b4f23f9a61d9d
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-01-14 12:03:45 +0530

    Update PageTransition.jsx

--- src/components/PageTransition.jsx
+++ src/components/PageTransition.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { PuffLoader } from 'react-spinners';
 
 const PageTransition = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [contentHeight, setContentHeight] = useState('100vh');
 
   const pageVariants = {
     initial: {
-      y: '100vh',
+      y: contentHeight,
       rotateX: 60,
       rotateY: 45,
       scale: 0.8,
@@ -26,7 +27,7 @@ const PageTransition = ({ children }) => {
     },
     exit: {
       opacity: 0,
-      y: '100vh',
+      y: contentHeight,
       rotateX: -60,
       rotateY: -45,
       scale: 0.8,
@@ -46,6 +47,9 @@ const PageTransition = ({ children }) => {
       setIsLoading(false);
     };
 
+    // Dynamically set contentHeight based on the actual content height
+    setContentHeight(`${document.body.scrollHeight}px`);
+
     return () => {
       handleExitComplete();
       handleAnimationComplete();
@@ -53,27 +57,31 @@ const PageTransition = ({ children }) => {
   }, []);
 
   return (
-    <div style={{ position: 'relative', height: '100vh', overflow: 'hidden' }}>
-      {isLoading && (
-        <div
-          style={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: 'white', // Set background color as needed
-            zIndex: 9999,
-          }}
-        >
-          <PuffLoader color="#36D7B7" />
-        </div>
-      )}
-
+    <div style={{ position: 'relative', height: contentHeight, overflow: 'hidden' }}>
       <AnimatePresence wait>
+        {isLoading && (
+          <motion.div
+            key="spinner"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            style={{
+              position: 'fixed',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'rgba(255, 255, 255, 0.9)',
+              zIndex: 9999,
+            }}
+          >
+            <PuffLoader color="#36D7B7" />
+          </motion.div>
+        )}
+
         <motion.div
           key={window.location.pathname}
           initial="initial"
