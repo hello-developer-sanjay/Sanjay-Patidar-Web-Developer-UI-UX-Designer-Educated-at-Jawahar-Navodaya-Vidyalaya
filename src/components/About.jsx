8aec90bddb7c6fa1c53d6e607658e820ef76ebf2
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-02-08 21:53:43 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,11 +14,51 @@ const slideIn = keyframes`
   }
 `;
 
+const CardWrapper = styled.div`
+  position: relative;
+  border: 2px solid #ff6b6b; 
+  border-radius: 20px;
+  padding: 20px;
+  margin-top: 100px;
+`;
+
+const Title = styled.h1`
+  position: absolute;
+  top: -40px;
+  left: 50%;
+  transform: translateX(-50%);
+  margin: 0;
+  font-size: 24px;
+  color: #fff; /* Text color */
+  background-color: #333; /* Background color for the "metal plate" */
+  padding: 10px 20px; /* Padding to create space around the text */
+  border-radius: 20px; /* Rounded corners for the "metal plate" */
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.3); /* Shadow to make it pop */
+  text-shadow: 0 1px 2px rgba(0, 0, 0, 0.5); /* Text shadow for depth */
+  border: 2px solid #ff6b6b; /* Border color */
+  overflow: hidden; /* Hide any overflowing text */
+  z-index: 1; /* Ensure the title appears above other elements */
+  
+  /* Engraved effect */
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: linear-gradient(to bottom, rgba(255, 255, 255, 0.2) 0%, transparent 50%, rgba(0, 0, 0, 0.5) 100%);
+    pointer-events: none; /* Allow interaction with elements behind the pseudo-element */
+  }
+`;
+
+
+
 const CardContainer = styled.div`
   width: 300px;
   height: auto;
   border-radius: 20px;
-  margin: 20px;
+  margin: 20px 10px;
   overflow: hidden;
   cursor: pointer;
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
@@ -27,6 +67,10 @@ const CardContainer = styled.div`
 
   &:hover {
     box-shadow: 0 15px 30px rgba(0, 0, 0, 0.3);
+  }
+  @media (max-width: 600px) {
+    margin: 20px 0;
+
   }
 `;
 
@@ -87,11 +131,9 @@ const BlogTitle = styled.h3`
   }
 `;
 
-
-
 const About = () => {
   const [randomBlogTitles, setRandomBlogTitles] = useState([]);
-  const [ setAnimationTrigger] = useState(false);
+  const [setAnimationTrigger] = useState(false);
 
   useEffect(() => {
     const fetchRandomBlogTitles = async () => {
@@ -108,8 +150,8 @@ const About = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Curated Blog Recommendations</h1>
+    <CardWrapper> 
+      <Title>Curated Blog Recommendations</Title>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {randomBlogTitles.map((title, index) => (
           <Link key={index} to={`https://eduxcel.vercel.app/blogs/tools/${encodeURIComponent(title)}`}>
@@ -127,7 +169,7 @@ const About = () => {
           </Link>
         ))}
       </div>
-    </div>
+    </CardWrapper>
   );
 };
 
