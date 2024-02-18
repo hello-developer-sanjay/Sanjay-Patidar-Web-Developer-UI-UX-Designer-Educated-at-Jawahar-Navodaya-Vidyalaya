fcef7022e35b4756e0cfe0cff17167f8217ba1f4
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-02-18 13:26:19 +0530

    Create useDisclosure.jsx

---
+++ src/components/useDisclosure.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+function useDisclosure(initialState = false) {
+  const [isOpen, setIsOpen] = useState(initialState);
+
+  const onOpen = () => setIsOpen(true);
+  const onClose = () => setIsOpen(false);
+  const onToggle = () => setIsOpen(!isOpen);
+
+  return {
+    isOpen,
+    onOpen,
+    onClose,
+    onToggle
+  };
+}
+
+export default useDisclosure;
