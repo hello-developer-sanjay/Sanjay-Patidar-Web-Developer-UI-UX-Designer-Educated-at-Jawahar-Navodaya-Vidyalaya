5979e7b006138f6dd0488fe842555755583375c2
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-02-18 10:47:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,7 @@ export default defineConfig({
   plugins: [
     react()
   ],
-  build: {
-    rollupOptions: {
-      external: /^@emotion\/styled/
-    }
-  },
-  optimizeDeps: {
-    exclude: ['@emotion/styled']
+  define: {
+    '@emotion/styled': 'styled'
   }
 });
