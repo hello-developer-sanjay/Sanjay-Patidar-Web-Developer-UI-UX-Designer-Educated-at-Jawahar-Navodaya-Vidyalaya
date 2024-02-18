27c0980bce57d2a130acab72670442c1946dfdad
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-02-18 21:05:38 +0530

    Update Career.jsx

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -761,6 +761,27 @@ cursor: "pointer",
                     <div id={`content-${blog.title}-overview`} style={contentSectionStyle}>
                       {renderMediaContent(blog.overview, blog.title)}
                     </div>
+
+                        <div id={`content-${blog.title}-design`} style={contentSectionStyle}>
+                      {renderMediaContent(blog.responsibilities?.design, blog.title)}
+                    </div>
+
+                    <div id={`content-${blog.title}-development`} style={contentSectionStyle}>
+                      {renderMediaContent(blog.responsibilities?.development, blog.title)}
+                    </div>
+                    <div id={`content-${blog.title}-maintenance`} style={contentSectionStyle}>
+                      {renderMediaContent(blog.responsibilities?.maintenance, blog.title)}
+                    </div>
+
+                    <div id={`content-${blog.title}-communication`} style={contentSectionStyle}>
+                      {renderMediaContent(blog.responsibilities?.communication, blog.title)}
+                    </div>
+
+                    <div id={`content-${blog.title}-continous_learning`} style={contentSectionStyle}>
+                      {renderMediaContent(blog.responsibilities?.continous_learning, blog.title)}
+                    </div>
+
+                  
                     <div id={`content-${blog.title}-description`} style={contentSectionStyle}>
                       {renderMediaContent(blog.description, blog.title)}
                     </div>
