c10d6522bf8429f3960a918d3d4bb2641e8f2ba8
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-03-03 10:56:45 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,31 +1,50 @@
 import  { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { FaBars, FaTimes, FaHome, FaFolder } from "react-icons/fa";
+import { FaBars, FaTimes, FaHome } from "react-icons/fa";
 import axios from "axios";
 import styled from "styled-components";
 const SidebarContainer = styled.nav`
-  background-color: #20232a;
+  background-color: #050816;
   padding: 20px;
-  width: 200px; /* Increased width for more content */
-  height: 100vh; /* Full height of the viewport */
+  width: 200px;
+  height: 100vh;
   color: white;
-  transition: transform 0.3s ease-in-out;
-  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-100%)")};
+  transition: transform 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+  transform: ${({ open }) => (open ? "translateY(0)" : "translateY(-150%)")};
   position: fixed;
   top: 70px;
   left: 0;
   z-index: 1000;
-  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5); /* Shadow effect */
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5);
   display: flex;
   flex-direction: column;
   align-items: center;
-   overflow-y: auto; 
-  max-height: 100vh; 
+  overflow-y: auto;
+  max-height: 100vh;
 
+  /* Add custom scrollbar styles */
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: #050816;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #ffcc29;
+    border-radius: 10px;
+    border: 2px solid #050816;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: #ffcc29;
+  }
+  
   /* Add creative animations and styles */
   &:hover {
-    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-95%)")};
-    transition: transform 0.3s ease-in-out;
+    transform: ${({ open }) => (open ? "translateY(0)" : "translateY(-90%)")};
+    transition: transform 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
   }
 
   a {
@@ -36,7 +55,7 @@ const SidebarContainer = styled.nav`
     transition: color 0.3s ease-in-out;
 
     &:hover {
-      color: #ffcc29;
+      color: #fff;
     }
   }
 
@@ -56,9 +75,10 @@ const SidebarContainer = styled.nav`
 `;
 
 
+
 const SidebarLink = styled(Link)`
   text-decoration: none;
-  color: white;
+  color: #050816;
   margin-bottom: 10px;
   transition: opacity 0.3s;
   display: flex;
@@ -68,7 +88,7 @@ const SidebarLink = styled(Link)`
   border-radius: 4px;
 
   &:hover {
-    background-color: #8AB008 ;
+    background-color: #050816 ;
     opacity: 0.9;
   }
 `;
@@ -118,7 +138,7 @@ const SidebarToggle = styled.div`
   position: sticky;
   top:0rem;
   right: 1rem;
-  font-size: 1rem;
+  font-size: 0.8rem;
   cursor: pointer;
   color: #ffcc29; /* Yellow color */
   background: #20232a; /* Background color */
