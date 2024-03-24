c12a7fa57dd3fcd1a021fa2c87ce2a2c405eada5
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-03-24 12:52:31 +0530

    Update BlogSuggestion.jsx

--- src/components/BlogSuggestion.jsx
+++ src/components/BlogSuggestion.jsx
@@ -224,7 +224,7 @@ const BlogSuggestion = () => {
   useEffect(() => {
     const fetchRandomBlogTitles = async () => {
       try {
-        const response = await axios.get('https://edu-back-j3mz.onrender.com/api/random-blog-titles');
+        const response = await axios.get('https://eduxcel-api3.onrender.com/api/random-blog-titles');
         setRandomBlogTitles(response.data);
         setAnimationTrigger(true); // Trigger animation after data fetch
       } catch (error) {
