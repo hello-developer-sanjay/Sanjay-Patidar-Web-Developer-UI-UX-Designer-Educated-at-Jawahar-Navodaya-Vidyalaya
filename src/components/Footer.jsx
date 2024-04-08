aaa67745544eea41edccf89d83fc95da8fa310d5
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-04-08 22:59:02 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,19 @@
-import { useEffect, useRef } from 'react';
+import  { useEffect } from 'react';
+import profileImage1 from '../assets/ssss.webp';
+
 import styled, { keyframes } from 'styled-components';
-import { motion, AnimatePresence } from "framer-motion";
-import { FaLinkedin, FaTwitter, FaInstagram, FaGithub, FaUsers, FaHome, FaBlog, FaBriefcase, FaUserCog, FaChalkboardTeacher, FaFileDownload, FaEnvelope } from "react-icons/fa";
+import { motion, AnimatePresence,  } from "framer-motion";
+import { FaLinkedin,FaUserTie ,FaUserGraduate , FaTwitter, FaInstagram, FaGithub, FaUsers, FaHome, FaBlog, FaBriefcase, FaUserCog, FaChalkboardTeacher, FaFileDownload, FaEnvelope } from "react-icons/fa";
+
 import { toast, ToastContainer } from "react-toastify";
-import "../styles/Sidebar.css";
-import { useState,  } from 'react';
-import {
-  FaUserGraduate,
-  FaUserTie,
-
-} from 'react-icons/fa';
-
 import "react-toastify/dist/ReactToastify.css";
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+
 const FooterContainer = styled(motion.footer)`
-  position: relative;
+   position: relative;
   color: white;
-  padding: 3rem;
+  padding: 1rem;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -33,8 +30,17 @@ const FooterContainer = styled(motion.footer)`
   
   /* Optional: Add animation or transition properties for a dynamic effect */
   transition: background 0.3s ease-in-out;
-    
-  `;
+`;
+const Text = styled.h1`
+  margin-top: 0rem;;
+  font-size: 1.1rem;
+  text-align: left;
+  letter-spacing: 0.2px; 
+  color: #fff; 
+  padding: 2px 5px; /* Padding to create space around the text */
+
+`;
+
 const BorderLineTop = styled(motion.div)`
   position: absolute;
   top: 0;
@@ -60,7 +66,6 @@ const BorderLineBottom = styled(BorderLineTop)`
   top: auto;
   bottom: 0;
 `;
-
 
 const bounceAnimation = keyframes`
   0%, 20%, 50%, 80%, 100% {
@@ -120,7 +125,6 @@ const CatchyMessage = styled(motion.p)`
   /* Combine animations */
   animation: ${bounceAnimation} 1s infinite, ${fadeIn} 1s ease-in-out, ${neonGlow} 1s infinite;
 `;
-
 const SocialIconsContainer = styled(motion.div)`
   display: flex;
   justify-content: center;
@@ -204,13 +208,14 @@ const ContactForm = styled(motion.form)`
   flex-direction: column;
   align-items: center;
   gap: 1.5rem;
-  margin-top: 1.5rem;
+  margin-top: 0.5rem;
   padding: 1.5rem;
   background-color: rgba(0, 0, 0, 0.2);
   border-radius: 15px;
   width: 100%;
   max-width: 500px;
-  border: 2px solid #ff6b6b; 
+
+    border: 2px solid #ff6b6b; 
 
 `;
 
@@ -220,7 +225,7 @@ const ContactInput = styled.input`
   border-radius: 5px;
   width: 100%;
   font-size: 1rem;
-  color: green;
+  color: white;
   background-color: rgba(0, 0, 0, 0.5);
   transition: background-color 0.3s ease;
 
@@ -228,6 +233,159 @@ const ContactInput = styled.input`
     background-color: rgba(0, 0, 0, 0.7);
   }
 `;
+
+const NavigationContainer = styled.nav`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+gap : 2rem;
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+    width: 60%;
+    margin: 0 auto;
+  }
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+const ProfileImageContainer = styled.div`
+  flex-shrink: 0;
+
+  @media (min-width: 768px) {
+    order: 1;
+    margin-right: 2rem;
+
+    align-self: flex-start; /* Align the image to the start of the container on larger screens */
+  }
+  
+`;
+const ProfileImage = styled(motion.img)`
+  width: 350px;
+  height: 350px;
+  margin-top: 2rem;
+  margin-left: 10rem;
+
+  border-radius: 50%;
+  box-shadow: 0 0 10px rgba(255, 165, 0, 0.8), 0 0 20px rgba(255, 165, 0, 0.6);
+  transform-origin: center;
+  animation: heartbeat 1.5s infinite, rotateAndGlow 8s infinite, bounce 2s alternate infinite;
+
+  &.loading {
+    border: 2px solid transparent;
+    position: relative;
+
+    &:before {
+      content: '';
+      position: absolute;
+      top: -2px;
+      left: -2px;
+      right: -2px;
+      bottom: -2px;
+      z-index: 1;
+      border-radius: 50%;
+      border: 2px solid #fff; // Change the color as needed
+      animation: loadingAnimation 1.5s linear infinite;
+    }
+
+    &:after {
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 0;
+      height: 100%;
+      border-left: 2px dashed #fff; // Change the color as needed
+      animation: loadingLineAnimation 1.5s linear infinite;
+    }
+  }
+
+  @keyframes loadingAnimation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes loadingLineAnimation {
+    0% {
+      height: 0;
+    }
+    50% {
+      height: 100%;
+    }
+    100% {
+      height: 0;
+    }
+  }
+
+
+
+
+  @media (max-width: 768px) {
+    width: 180px;
+    height: 180px;
+  margin-top : 2rem;
+    margin-left: 3rem;
+
+}
+
+  @keyframes heartbeat {
+    0%, 100% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.1);
+    }
+  }
+
+  @keyframes rotateAndGlow {
+    0%, 100% {
+      transform: rotate(0deg) scale(1);
+      box-shadow: 0 0 10px rgba(255, 165, 0, 0.8), 0 0 20px rgba(255, 165, 0, 0.6);
+    }
+    25% {
+      transform: rotate(90deg) scale(1.2);
+      box-shadow: 0 0 15px rgba(255, 165, 0, 0.9), 0 0 30px rgba(255, 165, 0, 0.7);
+    }
+    50% {
+      transform: rotate(180deg) scale(1);
+      box-shadow: 0 0 10px rgba(255, 165, 0, 0.8), 0 0 20px rgba(255, 165, 0, 0.6);
+    }
+    75% {
+      transform: rotate(270deg) scale(1.2);
+      box-shadow: 0 0 15px rgba(255, 165, 0, 0.9), 0 0 30px rgba(255, 165, 0, 0.7);
+    }
+  }
+
+  @keyframes bounce {
+    0%, 100% {
+      transform: translateY(0);
+    }
+    50% {
+      transform: translateY(-15px);
+    }
+  }
+`;
+
+
+
 const ContactTextArea = styled.textarea`
   padding: 1.5rem;
   border: none;
@@ -345,43 +503,102 @@ const QueryButton = styled(SubmitButton)`
     }
   }
 `;
-const Text = styled.h1`
-  margin-top: 0rem;;
-  font-size: 1.1rem;
-  text-align: left;
-  letter-spacing: 0.2px; 
-  color: #fff; 
-  padding: 2px 5px; /* Padding to create space around the text */
+const Next = styled.h1`
+font-size: 1.1rem;
+color: #f3f3f3;
+margin-bottom: 1.5rem;
+line-height: 1.4;
+text-align: justify;
+border-left: 4px solid #5d00ff;
+border-right: 4px solid #5d00ff;
 
+padding-left: 2px;
+padding-right:2px;
+border-radius: 8px;
+box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
 `;
-const NavigationContainer = styled.nav`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 
-  @media (min-width: 768px) {
-    flex-direction: row;
-    justify-content: space-between;
-    width: 60%;
-    margin: 0 auto;
+const Onlyforlap = styled.div`
+
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+margin-right : 1rem;
+margin-left: 1rem;
+
+  @media (max-width: 768px) {
+    margin-top: 0rem;
+  
   }
 `;
+const Introduction = styled(motion.p)`
+  font-size: 1.5rem;
+  line-height: 1.5;
+  max-width: 800px;
+  text-align: center;
 
-const Column = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+  color: #ffffff; /* White on hover */
+  font-family: 'Playfair Display', serif;
 
-const Link = styled(NavLink)`
-  text-decoration: none;
-  color: #333;
-  margin: 5px 0;
+  
+  .highlight {
+    position: relative;
+    display: inline-block;
+    font-size: 3rem;
+    font-weight: bold;
+    color: transparent;
+    font-family: 'Playfair Display', serif;
 
-  &:hover {
-    color: #666;
+    background: linear-gradient(45deg, #ff4081, #00bcd4); /* Gradient highlight */
+    background-clip: text;
+    -webkit-background-clip: text;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3); /* Shadow for depth */
+    padding-bottom: 0px;
+    /* Animation for the highlight class */
+    animation: highlightAnimation 3s ease-in-out infinite;
+    @media (max-width: 768px) {
+  font-size: 1.5rem;
+  line-height: 2rem;
+
+  }
+  }
+
+
+  @keyframes highlightAnimation {
+    0%, 100% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+  }
+
+  
+  &:after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 2px;
+    background: linear-gradient(45deg, #ff4081, #00bcd4); /* Gradient border */
+    margin-top: 0px;
+    position: relative;
+    animation: shimmerAnimation 3s ease-in-out infinite;
+  }
+
+  
+  @keyframes shimmerAnimation {
+    0% {
+      background-position: -200% 50%;
+    }
+    100% {
+      background-position: 200% 50%;
+    }
+  }
+   @media (max-width: 768px) {
+  margin-top:1rem;
+  font-size: 1.2rem;
+
   }
 `;
-
 const NavHeading = styled.h2`
   color: #333;
   font-size: 1.2rem;
@@ -389,13 +606,49 @@ const NavHeading = styled.h2`
 `;
 
 
+
+
+
+
 const Footer = () => {
   const catchyMessages = [
     "Stay Curious. Connect with Us!",
-    "Join the Journey. Let's Learn Together.",
     "Exploring the Future. Get Involved!",
-    "Unlock Knowledge. Engage and Share.",
   ];
+  const [isToastVisible, setIsToastVisible] = useState(false);
+  const copyContactNumber = () => {
+    const contactNumber = document.getElementById('contactNumber');
+    const range = document.createRange();
+    range.selectNode(contactNumber);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+    alert('Contact number copied!');
+  };
+  const copyInstaID = () => {
+    const instaID = document.getElementById('instaID');
+    const range = document.createRange();
+    range.selectNode(instaID);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+    alert('Instagram ID copied!');
+  };
+  const handleCareerInsightsClick = () => {
+    if (!isToastVisible) {
+      toast.info("Please wait! You're now being redirected to delve into Blog insights on Sanjay Patidar's Portfolio Website...", {
+        autoClose: 3000,
+        onOpen: () => setIsToastVisible(true),
+        onClose: () => setIsToastVisible(false),
+      });
+
+      setTimeout(() => {
+        window.open("https://sanjay-patidar.vercel.app/blogs", "_blank");
+      }, 3000); 
+    }
+  };
   const getCurrentDate = () => {
     const currentDate = new Date();
     const month = currentDate.toLocaleString('default', { month: 'long' });
@@ -404,69 +657,53 @@ const Footer = () => {
     return `${month} ${day}, ${year}`;
   };
 
-  const [isToastVisible, setIsToastVisible] = useState(false);
-
-  const handleCareerInsightsClick = () => {
-    if (!isToastVisible) {
-      toast.info("You're being redirected to explore career insights at Eduxcel...", {
-        autoClose: 2000,
-        onOpen: () => setIsToastVisible(true),
-        onClose: () => setIsToastVisible(false),
-      });
-
-      setTimeout(() => {
-        window.open("https://eduxcel.vercel.app/careers", "_blank");
-      }, 3000); 
-    }
+useEffect(() => {
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.4,
   };
 
-const footerRef = useRef(null);
-  useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.4,
-    };
-  
-    const callback = (entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          toast.info(
-            "Hey there! If you have feedback or a question for the admin, feel free to submit them separately. We appreciate your input!",
-            {
-              position: "top-right", 
-              autoClose: 10000, 
-              hideProgressBar: false, 
-              closeOnClick: true,
-              pauseOnHover: true, 
-              draggable: true, 
-              progress: undefined, 
-              style: {
-                background: "#487503", 
-                color: "#fff", 
-                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.4)", 
-                borderRadius: "10px", 
-              },
-            }
-          );
-          observer.unobserve(entry.target);
-        }
-      });
-    };
-  
-    const observer = new IntersectionObserver(callback, options);
-  
-    if (footerRef.current) {
-      observer.observe(footerRef.current);
-    }
-  
-    return () => {
-      if (footerRef.current) {
-        observer.unobserve(footerRef.current);
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        toast.info(
+          "Hey there! If you have feedback or a question for the admin, feel free to submit them separately. We appreciate your input!",
+          {
+            position: "top-right", 
+            autoClose: 10000, 
+            hideProgressBar: false, 
+            closeOnClick: true, 
+            pauseOnHover: true, 
+            draggable: true, 
+            progress: undefined,
+            style: {
+              background: "#487503", 
+              color: "#fff", 
+              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.4)", 
+              borderRadius: "10px", 
+            },
+          }
+        );
+        observer.unobserve(entry.target);
       }
-    };
-  }, [footerRef]);
-  
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  const footerElement = document.getElementById("footer");
+
+  if (footerElement) {
+    observer.observe(footerElement);
+  }
+
+  return () => {
+    if (footerElement) {
+      observer.unobserve(footerElement);
+    }
+  };
+}, []); // Make sure to i
   const getRandomCatchyMessage = () =>
     catchyMessages[Math.floor(Math.random() * catchyMessages.length)];
 
@@ -476,6 +713,7 @@ const footerRef = useRef(null);
     { icon: <FaTwitter />, label: "Twitter", link: "#" },
     { icon: <FaInstagram />, label: "Instagram", link: "https://www.instagram.com/sanjay_patidar_mcmxcviii" },
   ];
+  const [imageLoading, setImageLoading] = useState(true);
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
@@ -496,7 +734,7 @@ const footerRef = useRef(null);
     }
 
     try {
-      let endpoint = "submit-feedback";
+let endpoint = "submit-feedback";
       let successMessage = "Feedback submitted successfully! Thank you for your feedback.";
 
       if (query) {
@@ -504,7 +742,7 @@ const footerRef = useRef(null);
         successMessage = "Query sent! Await our swift reply, tailored just for you.";
       }
 
-      const response = await fetch(`https://eduxcel-api3-j9a2.onrender.com/api/${endpoint}`, {
+      const response = await fetch(`https://eduxcel-api3.onrender.com/api/${endpoint}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -532,7 +770,8 @@ const footerRef = useRef(null);
   };
 
   return (
-    <FooterContainer ref={footerRef}>
+    <FooterContainer id="footer">
+ 
       <BorderLineTop
         initial={{ width: 0 }}
         animate={{ width: "80%" }}
@@ -576,6 +815,62 @@ const footerRef = useRef(null);
           ))}
         </SocialIconsContainer>
       </AnimatePresence>
+
+      <FlexContainer>
+      <ProfileImageContainer>
+        
+        <ProfileImage
+  
+    src={profileImage1}
+    alt="jitendra patidar"
+    initial={{ y: -100, opacity: 0, filter: 'blur(10px)' }}
+    animate={{ y: 0, opacity: 1, filter: 'blur(0px)' }}
+    transition={{ type: "spring", damping: 10, stiffness: 100, delay: 0.5 }}
+    className={`profile-image ${imageLoading ? 'loading' : ''}`}
+    onLoad={() => {
+      setImageLoading(false);
+    }}
+    onError={() => {
+      setImageLoading(true); 
+    }}
+  />
+  
+
+          </ProfileImageContainer>
+      <Onlyforlap>
+
+  <Introduction>
+  <Next>
+  📞 Contact Sanjay Patidar Web Developer <span className="light">📞</span> {' '}
+  <button onClick={() => window.location.href = 'tel:+917987235207'} style={{ marginLeft: '4px', color: '#fff', padding: '2px 4px', border: '2px solid #ff6b6b', borderRadius: '30px', cursor: 'pointer', boxShadow: '0px 0px 10px #ffd700' }}>Call Sanjay Patidar</button>
+</Next>
+    <Next>
+    📞 Sanjay Patidar <span className="light">Contact | Mobile Number : </span>{' '}
+      <a href="tel:+919131743250" id="contactNumber" style={{ textDecoration: 'none', color: '#007bff', fontWeight: 'bold' }}>+91 9131743250</a> 📞 | OR |
+      
+      <button onClick={copyContactNumber} style={{ marginLeft: '4px', color: '#fff', padding: '2px 4px', border: '2px solid #ff6b6b', borderRadius: '30px', cursor: 'pointer' }}>Copy Number</button>
+    </Next>
+
+    <Next>
+  🔗 <span className="light">Sanjay Patidar Instagram ID : </span>{' '}
+  <a href="https://www.instagram.com/sanjay_patidar_mcmxcviii"style={{ textDecoration: 'none', color: '#007bff', fontWeight: 'bold' }} id="instaID" target="_blank">sanjay_patidar_mcmxcviii</a>
+  {' '} | OR | {' '}
+  <button onClick={copyInstaID} style={{ marginLeft: '4px', color: '#fff', padding: '2px 4px', border: '2px solid #ff6b6b', borderRadius: '30px', cursor: 'pointer' }}>Copy Insta ID</button>
+</Next>
+
+
+    <Next>
+      <span className="light">Based in Indore, Madhya Pradesh | Founder | Developer | Creator | Visionary | Innovator | Leader | <br /> | Entrepreneur | Technologist |</span><br />
+    </Next>
+    
+    <Text>➥ Curious to know more about EduXcel <a style={{ color: '#FAF7F7', padding: '2px 4px', border: '2px solid #ff6b6b', borderRadius: '30px', cursor: 'pointer', textDecoration: "none" }} href="https://eduxcel.vercel.app" target="_blank">EduXcel WebLink</a> to explore!</Text>
+  </Introduction>
+</Onlyforlap>
+</FlexContainer>
+
+
+  
+
       <ContactForm
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
@@ -598,8 +893,6 @@ const footerRef = useRef(null);
         Submit
       </QueryButton>
       </ContactForm>
-      <ToastContainer className="custom-toast-container" position="top-right" />
-
       <NavigationContainer>
   <Column>
     <NavHeading>Main</NavHeading>
@@ -655,8 +948,18 @@ const footerRef = useRef(null);
   </Column>
 </NavigationContainer>
 
- 
-<Text>Eduxcel provides a diverse range of courses tailored just for you. Don't miss out—register now to discover your perfect fit! <a style={{ color: '#FAF7F7', padding: '2px 4px', border: '2px solid #ff6b6b', borderRadius: '30px', cursor: 'pointer', textDecoration: 'none' }} href='https://eduxcel.vercel.app/signup' target='_blank'>SignUp EduXcel</a> to secure your spot!</Text>
+
+
+
+      <ToastContainer
+  className="custom-toast-container"
+  position="top-right"
+  style={{ marginTop: '100px' }}
+/>
+
+
+
+
 
 
 <Text>
@@ -668,8 +971,6 @@ const footerRef = useRef(null);
   <span style={{ fontWeight: 'bold', color: '#ffbb00' }}>Sanjay Patidar</span><br />
   <span style={{ fontSize: '0.9rem', color: '#ccc' }}>{getCurrentDate()} | India</span>
 </Text>
-
-
     </FooterContainer>
   );
 };
