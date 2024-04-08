3706d253baecf2d11c469a4a1daa81d37e1e09e0
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-04-08 19:01:47 +0530

    Update ProjectDetails.jsx

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -296,7 +296,7 @@ const ProjectDetails = () => {
   useEffect(() => {
     async function fetchProjectDetails() {
       try {
-        const response = await axios.get(`https://portfolio-api-b53a.onrender.com/api/project/${title}`);
+        const response = await axios.get(`https://portfolio-back-aruc.onrender.com/api/project/${title}`);
         setProject(response.data);
         setLoading(false);
       } catch (error) {
