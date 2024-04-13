88c5c069c6aeda1eacc520b3a084b09079d19db6
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-04-13 21:32:32 +0530

    Update BlogPage.jsx

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -67,7 +67,7 @@ const BlogPage = () => {
     useEffect(() => {
         async function fetchCareers() {
             try {
-                const response = await axios.get('https://portfolio-forblogs-zdl0.onrender.com/api/journals');
+                const response = await axios.get('https://portfolio-api-13april.onrender.com/api/journals');
                 setCareerData(response.data);
             } catch (error) {
                 console.error('Error fetching courses:', error);
