663d004d9108f26aa26ecf84df8dd137dc60457c
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-04-29 21:50:36 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
+/* eslint-disable react/jsx-no-target-blank */
 import  { useEffect } from 'react';
-import profileImage1 from '../assets/ssss.webp';
-
 import styled, { keyframes } from 'styled-components';
 import { motion, AnimatePresence,  } from "framer-motion";
 import { FaLinkedin,FaUserTie ,FaUserGraduate , FaTwitter, FaInstagram, FaGithub, FaUsers, FaHome, FaBlog, FaBriefcase, FaUserCog, FaChalkboardTeacher, FaFileDownload, FaEnvelope } from "react-icons/fa";
 
-import { toast, ToastContainer } from "react-toastify";
+import { toast,  } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
@@ -203,9 +202,6 @@ const FooterButton = styled(motion.button)`
   margin-top: 1rem;
 `;
 
-
-
-
 const NavigationContainer = styled.nav`
   display: flex;
   flex-direction: column;
@@ -224,363 +220,11 @@ const Column = styled.div`
   flex-direction: column;
 `;
 
-const FlexContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  text-align: center;
-  @media (min-width: 768px) {
-    flex-direction: row;
-  }
-`;
-const ProfileImageContainer = styled.div`
-  flex-shrink: 0;
-
-  @media (min-width: 768px) {
-    order: 1;
-    margin-right: 2rem;
-
-    align-self: flex-start; /* Align the image to the start of the container on larger screens */
-  }
-  
-`;
-const ProfileImage = styled(motion.img)`
-  width: 350px;
-  height: 350px;
-  margin-top: 2rem;
-  margin-left: 10rem;
-
-  border-radius: 50%;
-  box-shadow: 0 0 10px rgba(255, 165, 0, 0.8), 0 0 20px rgba(255, 165, 0, 0.6);
-  transform-origin: center;
-  animation: heartbeat 1.5s infinite, rotateAndGlow 8s infinite, bounce 2s alternate infinite;
-
-  &.loading {
-    border: 2px solid transparent;
-    position: relative;
-
-    &:before {
-      content: '';
-      position: absolute;
-      top: -2px;
-      left: -2px;
-      right: -2px;
-      bottom: -2px;
-      z-index: 1;
-      border-radius: 50%;
-      border: 2px solid #fff; // Change the color as needed
-      animation: loadingAnimation 1.5s linear infinite;
-    }
-
-    &:after {
-      content: '';
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      width: 0;
-      height: 100%;
-      border-left: 2px dashed #fff; // Change the color as needed
-      animation: loadingLineAnimation 1.5s linear infinite;
-    }
-  }
-
-  @keyframes loadingAnimation {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-
-  @keyframes loadingLineAnimation {
-    0% {
-      height: 0;
-    }
-    50% {
-      height: 100%;
-    }
-    100% {
-      height: 0;
-    }
-  }
-
-
-
-
-  @media (max-width: 768px) {
-    width: 180px;
-    height: 180px;
-  margin-top : 2rem;
-    margin-left: 3rem;
-
-}
-
-  @keyframes heartbeat {
-    0%, 100% {
-      transform: scale(1);
-    }
-    50% {
-      transform: scale(1.1);
-    }
-  }
-
-  @keyframes rotateAndGlow {
-    0%, 100% {
-      transform: rotate(0deg) scale(1);
-      box-shadow: 0 0 10px rgba(255, 165, 0, 0.8), 0 0 20px rgba(255, 165, 0, 0.6);
-    }
-    25% {
-      transform: rotate(90deg) scale(1.2);
-      box-shadow: 0 0 15px rgba(255, 165, 0, 0.9), 0 0 30px rgba(255, 165, 0, 0.7);
-    }
-    50% {
-      transform: rotate(180deg) scale(1);
-      box-shadow: 0 0 10px rgba(255, 165, 0, 0.8), 0 0 20px rgba(255, 165, 0, 0.6);
-    }
-    75% {
-      transform: rotate(270deg) scale(1.2);
-      box-shadow: 0 0 15px rgba(255, 165, 0, 0.9), 0 0 30px rgba(255, 165, 0, 0.7);
-    }
-  }
-
-  @keyframes bounce {
-    0%, 100% {
-      transform: translateY(0);
-    }
-    50% {
-      transform: translateY(-15px);
-    }
-  }
-`;
-
-
-
-const ContactTextArea = styled.textarea`
-  padding: 1.5rem;
-  border: none;
-  border-radius: 10px;
-  width: 100%;
-  resize: vertical;
-  font-size: 1rem;
-  color: #fff;
-  background-color: #1a1a1a;
-  transition: box-shadow 0.3s ease;
-
-  &::placeholder {
-    color: #666;
-  }
-
-  &:hover, &:focus {
-    box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.3);
-  }
-
-  // Add a subtle pulsating animation on hover
-  &:hover::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(255, 255, 255, 0.05);
-    border-radius: 10px;
-    animation: pulse 1.5s cubic-bezier(0.175, 0.885, 0.32, 1.275) infinite;
-  }
-
-  @keyframes pulse {
-    0%, 100% {
-      transform: scale(1);
-      opacity: 1;
-    }
-    50% {
-      transform: scale(1.05);
-      opacity: 0.8;
-    }
-  }
-`;
-const SubmitButton = styled.button`
-  padding: 0.5rem 1rem;
-  background-color: #ffbb00;
-  border: none;
-  border-radius: 5px;
-  color: white;
-  cursor: pointer;
-  font-weight: bold;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: #e85d04;
-  }
-`;
-
-const QueryInput = styled(ContactTextArea)`
-  // Additional styling for query input
-`;
-const QueryButton = styled(SubmitButton)`
-  // Base styles from SubmitButton
-  background: linear-gradient(135deg, #2c3e50, #34495e);
-  color: white;
-  border: none;
-  border-radius: 30px;
-  padding: 10px 25px;
-  font-size: 1.3rem;
-  cursor: pointer;
-  font-family: 'Roboto', sans-serif;
-  transition: background 0.3s ease, transform 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-
-  // Additional styling specific to QueryButton
-  margin-top: 2rem;
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.4);
-  text-transform: uppercase;
-  letter-spacing: 1px;
-  position: relative;
-  overflow: hidden;
-
-  // Shining gradient border effect
-  border: 2px solid transparent;
-  background-clip: padding-box;
-  background-image: linear-gradient(135deg, #e74c3c, #3498db);
-  transition: border 0.3s ease;
-
-  &:hover {
-    background: linear-gradient(135deg, #2c3e50, #2c3e50);
-    transform: translateY(-3px) scale(1.05);
-    border: 2px solid #e74c3c;
-  }
-
-  // Add a subtle pulse animation on hover
-  &:hover:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(255, 255, 255, 0.1);
-    border-radius: 50%;
-    animation: pulse 1s cubic-bezier(0.175, 0.885, 0.32, 1.275) infinite;
-  }
-
-  @keyframes pulse {
-    0%, 100% {
-      transform: scale(1);
-      opacity: 1;
-    }
-    50% {
-      transform: scale(1.2);
-      opacity: 0;
-    }
-  }
-`;
-const Next = styled.h1`
-font-size: 1.1rem;
-color: #f3f3f3;
-margin-bottom: 1.5rem;
-line-height: 1.4;
-text-align: justify;
-border-left: 4px solid #5d00ff;
-border-right: 4px solid #5d00ff;
-
-padding-left: 2px;
-padding-right:2px;
-border-radius: 8px;
-box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-`;
-
-const Onlyforlap = styled.div`
-
-  margin-top: 2rem;
-  margin-bottom: 1rem;
-margin-right : 1rem;
-margin-left: 1rem;
-
-  @media (max-width: 768px) {
-    margin-top: 0rem;
-  
-  }
-`;
-const Introduction = styled(motion.p)`
-  font-size: 1.5rem;
-  line-height: 1.5;
-  max-width: 800px;
-  text-align: center;
-
-  color: #ffffff; /* White on hover */
-  font-family: 'Playfair Display', serif;
-
-  
-  .highlight {
-    position: relative;
-    display: inline-block;
-    font-size: 3rem;
-    font-weight: bold;
-    color: transparent;
-    font-family: 'Playfair Display', serif;
-
-    background: linear-gradient(45deg, #ff4081, #00bcd4); /* Gradient highlight */
-    background-clip: text;
-    -webkit-background-clip: text;
-    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3); /* Shadow for depth */
-    padding-bottom: 0px;
-    /* Animation for the highlight class */
-    animation: highlightAnimation 3s ease-in-out infinite;
-    @media (max-width: 768px) {
-  font-size: 1.5rem;
-  line-height: 2rem;
-
-  }
-  }
-
-
-  @keyframes highlightAnimation {
-    0%, 100% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-  }
-
-  
-  &:after {
-    content: '';
-    display: block;
-    width: 100%;
-    height: 2px;
-    background: linear-gradient(45deg, #ff4081, #00bcd4); /* Gradient border */
-    margin-top: 0px;
-    position: relative;
-    animation: shimmerAnimation 3s ease-in-out infinite;
-  }
-
-  
-  @keyframes shimmerAnimation {
-    0% {
-      background-position: -200% 50%;
-    }
-    100% {
-      background-position: 200% 50%;
-    }
-  }
-   @media (max-width: 768px) {
-  margin-top:1rem;
-  font-size: 1.2rem;
-
-  }
-`;
 const NavHeading = styled.h2`
   color: #333;
   font-size: 1.2rem;
   margin-bottom: 10px;
 `;
-
-
-
-
-
 
 const Footer = () => {
   const catchyMessages = [
@@ -588,26 +232,7 @@ const Footer = () => {
     "Exploring the Future. Get Involved!",
   ];
   const [isToastVisible, setIsToastVisible] = useState(false);
-  const copyContactNumber = () => {
-    const contactNumber = document.getElementById('contactNumber');
-    const range = document.createRange();
-    range.selectNode(contactNumber);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand('copy');
-    window.getSelection().removeAllRanges();
-    alert('Contact number copied!');
-  };
-  const copyInstaID = () => {
-    const instaID = document.getElementById('instaID');
-    const range = document.createRange();
-    range.selectNode(instaID);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand('copy');
-    window.getSelection().removeAllRanges();
-    alert('Instagram ID copied!');
-  };
+
   const handleCareerInsightsClick = () => {
     if (!isToastVisible) {
       toast.info("Please wait! You're now being redirected to delve into Blog insights on Sanjay Patidar's Portfolio Website...", {
@@ -685,7 +310,6 @@ useEffect(() => {
     { icon: <FaTwitter />, label: "Twitter", link: "#" },
     { icon: <FaInstagram />, label: "Instagram", link: "https://www.instagram.com/sanjay_patidar_mcmxcviii" },
   ];
-  const [imageLoading, setImageLoading] = useState(true);
 
  
   return (
@@ -727,7 +351,7 @@ useEffect(() => {
               href={button.link}
               target="_blank"
               rel="noopener noreferrer"
-              aria-label={button.label} // Provide an accessible label for the link
+              aria-label={button.label} 
             >
               {button.icon}
             </SocialIcon>
@@ -735,62 +359,6 @@ useEffect(() => {
         </SocialIconsContainer>
       </AnimatePresence>
 
-      <FlexContainer>
-      <ProfileImageContainer>
-        
-        <ProfileImage
-  
-    src={profileImage1}
-    alt="jitendra patidar"
-    initial={{ y: -100, opacity: 0, filter: 'blur(10px)' }}
-    animate={{ y: 0, opacity: 1, filter: 'blur(0px)' }}
-    transition={{ type: "spring", damping: 10, stiffness: 100, delay: 0.5 }}
-    className={`profile-image ${imageLoading ? 'loading' : ''}`}
-    onLoad={() => {
-      setImageLoading(false);
-    }}
-    onError={() => {
-      setImageLoading(true); 
-    }}
-  />
-  
-
-          </ProfileImageContainer>
-      <Onlyforlap>
-
-  <Introduction>
-  <Next>
-  📞 Contact Sanjay Patidar Web Developer <span className="light">📞</span> {' '}
-  <button onClick={() => window.location.href = 'tel:+919131743250'} style={{ marginLeft: '4px', color: '#000501', padding: '2px 4px', border: '2px solid #ff6b6b', borderRadius: '30px', cursor: 'pointer', boxShadow: '0px 0px 10px #ffd700' }}>Call Sanjay Patidar</button>
-</Next>
-    <Next>
-    📞 Sanjay Patidar <span className="light">Contact | Mobile Number : </span>{' '}
-      <a href="tel:+919131743250" id="contactNumber" style={{ textDecoration: 'none', color: '#007bff', fontWeight: 'bold' }}>+91 9131743250</a> 📞 | OR |
-      
-      <button onClick={copyContactNumber} style={{ marginLeft: '4px', color: '#000501', padding: '2px 4px', border: '2px solid #ff6b6b', borderRadius: '30px', cursor: 'pointer' }}>Copy Number</button>
-    </Next>
-
-    <Next>
-  🔗 <span className="light">Sanjay Patidar Instagram ID : </span>{' '}
-  <a href="https://www.instagram.com/sanjay_patidar_mcmxcviii"style={{ textDecoration: 'none', color: '#007bff', fontWeight: 'bold' }} id="instaID" target="_blank">sanjay_patidar_mcmxcviii</a>
-  {' '} | OR | {' '}
-  <button onClick={copyInstaID} style={{ marginLeft: '4px', color: '#000501', padding: '2px 4px', border: '2px solid #ff6b6b', borderRadius: '30px', cursor: 'pointer' }}>Copy Insta ID</button>
-</Next>
-
-
-    <Next>
-      <span className="light">Based in Indore, Madhya Pradesh | Founder | Developer | Creator | Visionary | Innovator | Leader | <br /> | Entrepreneur | Technologist |</span><br />
-    </Next>
-    
-    <Text>➥ Curious to know more about EduXcel <a style={{ color: '#FAF7F7', padding: '2px 4px', border: '2px solid #ff6b6b', borderRadius: '30px', cursor: 'pointer', textDecoration: "none" }} href="https://eduxcel.vercel.app" target="_blank">EduXcel WebLink</a> to explore!</Text>
-  </Introduction>
-</Onlyforlap>
-</FlexContainer>
-
-
-  
-
-   
       <NavigationContainer>
   <Column>
     <NavHeading>Main</NavHeading>
@@ -845,14 +413,6 @@ useEffect(() => {
     </NavLink>
   </Column>
 </NavigationContainer>
-
-
-
-
-
-
-
-
 
 
 <Text>
