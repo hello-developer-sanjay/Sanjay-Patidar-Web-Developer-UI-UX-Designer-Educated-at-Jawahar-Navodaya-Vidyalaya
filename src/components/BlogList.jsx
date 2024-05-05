05f083e2341a29db3fb3bf665dcfce2a82083cad
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-05-05 12:53:58 +0530

    Update BlogList.jsx

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -2,26 +2,44 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { RingLoader } from 'react-spinners'; // Import the RingLoader component
+
 import '../styles/BlogList.css';
 import courseImage1 from '../assets/vs_code_articles.jpg';
 import courseImage2 from '../assets/gitarticles.jpg';
+const LoadingOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.7); /* Semi-transparent white background */
+  z-index: 9999; /* Ensure it's above other elements */
+`;
 function BlogList() {
   const [journals, setJournals] = useState([]);
   const { vision } = useParams();
+  const [loading, setLoading] = useState(true); // State to manage loading
 
   useEffect(() => {
     async function fetchJournals() {
       try {
         let response;
         if (!vision || vision === 'all') {
-          response = await axios.get('https://portfolio-api-30april.onrender.com/api/vision/all');
+          response = await axios.get('https://portfolio-api-14april.onrender.com/api/vision/all');
         } else {
-          response = await axios.get(`https://portfolio-api-30april.onrender.com/api/vision/${vision}`);
+          response = await axios.get(`https://portfolio-api-14april.onrender.com/api/vision/${vision}`);
         }
         if (!response) {
-          response = await axios.get('https://portfolio-api-30april.onrender.com/api/vision');
+          response = await axios.get('https://portfolio-api-14april.onrender.com/api/vision');
         }
         setJournals(response.data);
+        setLoading(false); // Set loading to false when data is fetched
+        
       } catch (error) {
         console.error('Error fetching journals:', error);
       }
@@ -38,6 +56,11 @@ function BlogList() {
  
   return (
     <div className="course-list">
+       {loading && ( // Display loading animation if loading is true
+        <LoadingOverlay>
+          <RingLoader color="#13584F" loading={loading} size={150} />
+        </LoadingOverlay>
+      )}
       <div className="course-cards">
         {uniqueCategories.map((vision, index) => {
           const journal = journals.find(journal => journal.vision === vision);
