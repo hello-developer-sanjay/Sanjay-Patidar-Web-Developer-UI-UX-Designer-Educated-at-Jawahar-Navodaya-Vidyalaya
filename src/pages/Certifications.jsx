dfa9e7b01e289e7edc706fa573d1cfec351aa659
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-06-06 00:17:00 +0530

    Update Certifications.jsx

--- src/pages/Certifications.jsx
+++ src/pages/Certifications.jsx
@@ -15,7 +15,7 @@ const Certifications = () => {
     async function fetchCertifications() {
       try {
         const response = await axios.get(
-          "https://portfolio-api-30april.onrender.com/api/certifications"
+          "https://portfolio-api-5jun.onrender.com/api/certifications"
         );
         setCertifications(response.data);
         setLoading(false);
