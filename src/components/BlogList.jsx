75ded48aa9491408b41c9f861bda0ad2fcbb7325
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-07-16 14:23:15 +0530

    Update BlogList.jsx

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -30,12 +30,12 @@ function BlogList() {
       try {
         let response;
         if (!vision || vision === 'all') {
-          response = await axios.get('https://portfolio-api-26jun.onrender.com/api/vision/all');
+          response = await axios.get('https://portfolio-api-16july.onrender.com/api/vision/all');
         } else {
-          response = await axios.get(`https://portfolio-api-26jun.onrender.com/api/vision/${vision}`);
+          response = await axios.get(`https://portfolio-api-16july.onrender.com/api/vision/${vision}`);
         }
         if (!response) {
-          response = await axios.get('https://portfolio-api-26jun.onrender.com/api/vision');
+          response = await axios.get('https://portfolio-api-16july.onrender.com/api/vision');
         }
         setJournals(response.data);
         setLoading(false); // Set loading to false when data is fetched
